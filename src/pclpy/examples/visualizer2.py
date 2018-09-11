9cea8036661b257243a2f773a9b3daf84ecf134c
Ricardo Peralta <rcardo.peralta@gmail.com>
2018-09-11 10:55:41 -0500

    Agrego otro ejemplo para visualizador

---
+++ src/pclpy/examples/visualizer2.py
@@ -0,0 +1,15 @@
+from pclpy.visualization import PCLVisualizer
+
+vis = PCLVisualizer()
+
+cloud1 = loadPCDFile('inputCloud1.pcd')
+cloud2 = loadPCDFile('inputCloud2.pcd')
+
+id1 = vis.addPointCloud(cloud1)
+id2 = vis.addPointCloud(cloud2)
+
+vis.spin()
+
+vis.removePointCloud(id2)
+
+vis.spin()
