db3fd93f48d179831201f09685d3e728e6d3d320
Ricardo Peralta <rcardo.peralta@gmail.com>
2018-09-12 19:47:45 -0500

    doc y ejemplos para outlier

--- src/pclpy/examples/outlier.py
+++ src/pclpy/examples/outlier.py
@@ -2,16 +2,16 @@ from pclpy.pcdio import loadPCDFile
 from pclpy.visualization import PCLVisualizer
 from pclpy.filters import StatisticalOutlierRemoval
 
-cloud = loadPCDFile('inputCloud0.pcd')
+cloud = loadPCDFile('inputCloud.pcd')
 
-_sor = StatisticalOutlierRemoval()
-_sor.setInputCloud(cloud)
-_sor.setMeanK(100)
-_sor.setStddevMulThresh(1.0)
+sor = StatisticalOutlierRemoval()
+sor.setInputCloud(cloud)
+sor.setMeanK(100)
+sor.setStddevMulThresh(1.0)
 
-fcloud = _sor.filter()
+filtered_cloud = _sor.filter()
 
 vis = PCLVisualizer()
 
-vis.addPointCloud(fcloud)
+vis.addPointCloud(filtered_cloud)
 vis.spin()
--- src/pclpy/filters/_pcl_outliers_wrapper_py.py
+++ src/pclpy/filters/_pcl_outliers_wrapper_py.py
@@ -18,7 +18,7 @@ class StatisticalOutlierRemoval(object):
 		- cloud : cloud to filter
 		'''
 		if not isinstance( cloud, PointCloud2 ):
-			rospy.ROSException( 'Argument1 is not PointCloud2' )
+			rospy.ROSException( 'cloud is not PointCloud2' )
 
 		_cloud = self._to_cpp( cloud )
 
@@ -31,7 +31,7 @@ class StatisticalOutlierRemoval(object):
 		- nr_k : The number of points to use for mean distance estimation
 		'''
 		if not isinstance( nr_k, int ):
-			rospy.ROSException( 'Argument1 is not int' )
+			rospy.ROSException( 'nr_k is not int' )
 		_nr_k = self._to_cpp( Int32(nr_k) )
 		self._outliers.setMeanK( _nr_k )
 
@@ -42,7 +42,7 @@ class StatisticalOutlierRemoval(object):
 		- stddev_mult : The standard deviation multiplier
 		'''
 		if not isinstance( stddev_mult, float ):
-			rospy.ROSException( 'Argument1 is not float' )
+			rospy.ROSException( 'stddev_mult is not float' )
 
 		_stddev_mult = self._to_cpp( Float32(stddev_mult) )
 		self._outliers.setStddevMulThresh( _stddev_mult )
@@ -54,7 +54,7 @@ class StatisticalOutlierRemoval(object):
 		- negative : false = normal filter behavior (default), true = inverted behavior
 		'''
 		if not isinstance( negative, bool ):
-			rospy.ROSException( 'Argument1 is not bool' )
+			rospy.ROSException( 'negative is not bool' )
 		_neg = self._to_cpp( Bool(negative) )
 		self._outliers.setNegative( _neg )
 
